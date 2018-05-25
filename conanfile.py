d6479781ba116bbc857c435d3d4b9862516d645a
Enrico May <sharkfox1720@gmail.com>
2018-05-25 20:23:18 +0000

    Add source function to build from source

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,17 @@
 
 from conans import ConanFile, CMake
-import os
 
 class ConanFoo(ConanFile):
     name        = "foo"
     version     = "4.0.1"
     description = "This is the famous foo library."
-    url         = "http://example.com"
+    url         = "https://github.com/sharkfox/conan-foo-lib"
     license     = "proprietary"
     generators  = "cmake"
     settings    = "os", "compiler", "build_type", "arch"
+
+    def source(self):
+        self.run("git clone -b v%s %s.git ." % (self.version, self.url))
 
     def build(self):
         cmake = CMake(self)
